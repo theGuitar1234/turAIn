652f04ca11a9cd98ae6b88eec26b4c0051a5dca4
The Guitar <turanlizad776@gmail.com>
2026-04-18 17:42:16 +0400

    60cba166-c13b-41cf-a020-f56d3c3e4009

---
+++ src/main/turain/neural_network/activations/activation.py
@@ -0,0 +1,27 @@
+from main.turain.core.module import Module
+from lib import override_from_parent
+from utilities import core_method
+
+
+class Activation(Module):
+    def __init__(self, z, backend):
+        super.__init__()
+        self.backend = backend
+        self.input_cache = None
+        self.z = z
+
+    @core_method
+    def activate(self, z):
+        raise NotImplementedError
+    
+    @override_from_parent
+    def forward_propagation(self, x):
+        return super().forward_propagation(x)
+    
+    @override_from_parent
+    def backward_propagation(self, gradient_output):
+        return super().backward_propagation(gradient_output)
+    
+    @override_from_parent
+    def parameters(self):
+        return super().parameters()
--- src/main/turain/neural_network/activations/relu.py
+++ src/main/turain/neural_network/activations/relu.py
@@ -1,2 +1,30 @@
-class ReLu:
-    pass
+from main.turain.neural_network.activations.activation import Activation
+from lib import override_from_parent
+from utilities import core_method
+
+class ReLu(Activation):
+    def __init__(self, z):
+        super.__init__()
+        self.z = z
+    
+    @core_method
+    @override_from_parent
+    def activate(self, z):
+        xp = self.backend
+        z = xp.asarray(z, dtype=float)
+        return xp.maximum(0.0, z)
+    
+    @override_from_parent
+    def forward_propagation(self, x):
+        self.activate(x)
+    
+    @override_from_parent
+    def backward_propagation(self, gradient_output):
+        xp = self.backend()
+        x = self.input_cache
+        gradient_input = gradient_output * (x > 0)
+        return gradient_input
+    
+    @override_from_parent
+    def parameters(self):
+        return super().parameters()
